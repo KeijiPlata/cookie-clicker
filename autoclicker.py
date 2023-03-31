6d8020eee8528584261ac5f511888ad23f6e070e
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-31 23:04:39 +0800

    Add comments

--- autoclicker.py
+++ autoclicker.py
@@ -5,7 +5,6 @@ from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 
 # declaration of variables
-
 delay = 0.1
 button = Button.left
 toggle_key = KeyCode(char='a')
@@ -30,12 +29,15 @@ class ClickMouse(threading.Thread):
         self.program_running = True
 
     def start_clicking(self):
+        """start"""
         self.running = True
 
     def stop_clicking(self):
+        """stop"""
         self.running = False
 
     def exit(self):
+        """This will terminate the whole program"""
         self.stop_clicking()
         self.program_running = False
 
@@ -55,14 +57,16 @@ click_thread = ClickMouse(delay, button)
 click_thread.start()
 
 def on_presss(key):
-    
+    """start and stop the program"""
     if key == toggle_key:
+        # toggle key, if thread.running is true, it will stop and vice versa
         if click_thread.running:
             click_thread.stop_clicking()
         else:
             click_thread.start_clicking()
 
     elif key == stop_key:
+        # terminate the whole program
         click_thread.exit()
         listener.stop()
 
