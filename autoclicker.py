0daa452e24a3199136a9adea2a59cb4a5c366eb3
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-31 22:51:59 +0800

    first commit, add autoclicker.py

---
+++ autoclicker.py
@@ -0,0 +1,70 @@
+# import packages needed
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+# declaration of variables
+
+delay = 0.1
+button = Button.left
+toggle_key = KeyCode(char='a')
+stop_key = KeyCode(char='b')
+
+# extends the threading.Thread
+# inherit the importa
+class ClickMouse(threading.Thread):
+
+    # get the button and delay value
+    def __init__(self, delay, button):
+        """Initialize variables"""
+        # inherit
+        super(ClickMouse, self).__init__()
+
+        # get the the button and delay time
+        self.delay = delay
+        self.button = button
+
+        # declare a new variable
+        self.running = False
+        self.program_running = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        """Main loop"""
+        while self.program_running:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.0001)
+
+# creating the instance of the Controller class and recently made class
+mouse = Controller()
+click_thread = ClickMouse(delay, button)
+
+# starts the loop
+click_thread.start()
+
+def on_presss(key):
+    
+    if key == toggle_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+
+    elif key == stop_key:
+        click_thread.exit()
+        listener.stop()
+
+with Listener(on_press=on_presss) as listener:
+    listener.join()
